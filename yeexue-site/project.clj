c8be12d34984bcc824e0fbb98273bd37fd02a794
unionx <xudi1990@gmail.com>
2012-08-26 16:13:08 +0800

    add postgresql dependency

--- yeexue-site/project.clj
+++ yeexue-site/project.clj
@@ -3,7 +3,11 @@
   :url "http://example.com/FIXME"
   :dependencies [[org.clojure/clojure "1.4.0"]
                  [compojure "1.1.1"]
-                 [enlive "1.0.0"]]
+                 [enlive "1.0.0"]
+                 [org.clojure/tools.nrepl "0.2.0-beta9"]
+                 [org.clojure/java.jdbc "0.2.3"]
+                 [postgresql "9.1-901.jdbc4"]
+                 [clojureql "1.0.3"]]
   :plugins [[lein-ring "0.7.3"]]
   :ring {:handler edu-site.handler/app}
   :profiles
